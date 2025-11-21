3b9a70cd99e6140d952b60ae58e870e1fc92a21f
lianglonghui_i <lianglonghui_i>
2025-11-21 21:05:41 +0800

    feat: 开发首页及相关组件（未完成）

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,63 @@
-import { Text, View } from 'react-native'
+/*
+ * @Author: lianglonghui_i lianglonghui_i
+ * @Date: 2025-11-21 16:26:50
+ * @LastEditors: lianglonghui_i lianglonghui_i
+ * @LastEditTime: 2025-11-21 21:04:58
+ * @FilePath: /rn-app/app/(tabs)/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { View, Image, ScrollView, Text, ActivityIndicator, FlatList } from 'react-native'
+import { SearchBar } from '@/components/search-bar'
+import { useRouter } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import MovieCard from '@/components/movie-card'
 
 export default function Index() {
+  const router = useRouter()
+
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }))
   return (
-    <View className="flex-1 items-center justify-between ">
-      <Text className="text-accent">Init</Text>
+    <View className="flex-1 bg-primary ">
+      <Image className="absolute z-0 w-full" source={images.bg} />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: '100%',
+          paddingBottom: 10,
+        }}
+      >
+        <Image className="mx-auto mb-5 mt-20 h-10 w-12" source={icons.logo}></Image>
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000FF" className="mt-10 self-center" />
+        ) : moviesError ? (
+          <Text className="mb-3 mt-5 text-lg font-bold text-white">Error:{moviesError?.message}:</Text>
+        ) : (
+          <View className="flex-1 ">
+            <SearchBar placeholder="搜索电影" onPress={() => router.push('/search')} />
+            <>
+              <Text className="mb-3 mt-5 text-lg font-bold text-white">最新电影</Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard>{}</MovieCard>}
+                keyExtractor={item => item.id?.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: 'center',
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
+      </ScrollView>
     </View>
   )
 }
---
+++ components/search-bar.tsx
@@ -0,0 +1,31 @@
+/*
+ * @Author: lianglonghui_i lianglonghui_i
+ * @Date: 2025-11-21 17:24:43
+ * @LastEditors: lianglonghui_i lianglonghui_i
+ * @LastEditTime: 2025-11-21 17:52:46
+ * @FilePath: /rn-app/components/search-bar.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { icons } from '@/constants/icons'
+
+import { View, Image, TextInput } from 'react-native'
+
+interface SearchBarProps {
+  placeholder: string
+  onPress?: () => void
+}
+
+export const SearchBar = ({ placeholder, onPress }: SearchBarProps) => {
+  return (
+    <View className="flex-row items-center rounded-full bg-dark-200 px-5 py-4">
+      <Image className="size-5" source={icons.search} resizeMode="contain" tintColor="#AB8BFF" />
+      <TextInput
+        className="ml-2 flex-1 text-white"
+        onPress={onPress}
+        placeholder={placeholder}
+        value={''}
+        onChangeText={() => {}}
+      ></TextInput>
+    </View>
+  )
+}
